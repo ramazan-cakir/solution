30b4761c6672c632254fcd4549e9b9e5c324528d
ramazan-cakir <rmzncakirdim@gmail.com>
2023-03-12 00:27:37 +0300

    2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import './Header.scss'
+import "./Header.scss";
 
 const Header = () => {
   return (
     <div className="header">
-        <span className="text-shadow">Who Are You ?</span>
+      <span className="text-shadow">Who Are You ?</span>
     </div>
   )
 }
 
-export default Header
+export default Header;
